fc41b44520fa4ffb9dda5fb59353572072d4e7cd
afshinmhdz <afshinmhdz1906@gmail.com>
2024-07-30 00:30:20 +0330

    create [notes,setNotes] useState initial value an array for saveing

--- src/component/AddNewNote.jsx
+++ src/component/AddNewNote.jsx
@@ -1,27 +1,51 @@
 import { useState } from "react";
 import "../App.css";
 function AddNewNote() {
-    const [title,setTitle]=useState("");
-    const [description,setDescription]=useState("");
-    const handleSubmit=(e)=>{
-        e.preventDefault(); {/** for prevent refresh on first load */}
-        const newNote={
-            id:Date.now(),
-            title,
-            description,
-            completed:false,
-            createdAt:new Date().toISOString()
-        }
-        setTitle("");
-        setDescription("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [notes, setNotes] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    {
+      /** for prevent refresh on first load */
     }
+    const newNote = {
+      id: Date.now(),
+      title,
+      description,
+      completed: false,
+      createdAt: new Date().toISOString(),
+    };
+    setTitle("");
+    setDescription("");
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+      
+    });
+  };
   return (
     <div className="add-new-note">
       <h2>Add New Note</h2>
-      <form className="note-form" onSubmit={handleSubmit}>  {/*onSubmit Event for user press enter - data in input send to server*/} 
-        <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} className="text-field" placeholder="titel"/>
-        <input type="text" value={description} onChange={(e)=>setDescription(e.target.value)} className="text-field" placeholder="description"/>
-        <button type="submit" className="btn btn--primary">Add New Note</button>
+      <form className="note-form" onSubmit={handleSubmit}>
+        {" "}
+        {/*onSubmit Event for user press enter - data in input send to server*/}
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="text-field"
+          placeholder="titel"
+        />
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          className="text-field"
+          placeholder="description"
+        />
+        <button type="submit" className="btn btn--primary">
+          Add New Note
+        </button>
       </form>
     </div>
   );
