b54626177df364423903ea4391b33eab03ec517e
afshinmhdz <afshinmhdz1906@gmail.com>
2024-08-01 00:11:30 +0330

    lift state up from NoteHeader to parent App.jsx for using in NoteList component and write logic for NoteHeader component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import "../src/index.css";
 import AddNewNote from "./component/AddNewNote";
 import NoteList from "./component/NoteList";
 import NoteStatus from "./component/NoteStatus";
+import NoteHeader from "./component/NoteHeader";
 function App() {
   const [notes, setNotes] = useState([]);
+  const [sortby, setSortby] = useState("latest");
   const handleAddNote = (newNote) => {
     setNotes((prevNotes) => {
       return [...prevNotes, newNote];
@@ -22,15 +24,32 @@ function App() {
       )
     );
   };
+
+  let sortedNotes = notes;
+  if (sortby === "earliest")
+    sortedNotes=[...notes].sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+  {
+    /**[...notes] in this line for an shallow copy of array for not mutate state and we can write this line with another way => notes.slice().sort((a,b)=>new Date(a.createdAt)-new Date(b.createdAt));  */
+  }
+  if (sortby === "latest")
+    sortedNotes=[...notes].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+  if (sortby === "completed")
+    sortedNotes=[...notes].sort((a, b) => Number((a.completed)) - Number((b.completed)));
+
   return (
     <>
       <div className="container">
-        <div className="note-header">
+        <div>
+          <NoteHeader
+            notes={notes}
+            sortby={sortby}
+            onSort={(e) => setSortby(e.target.value)}
+          />
         </div>
         <div className="note-app">
           <AddNewNote onAddNote={handleAddNote} />
           <div className="note-container">
-        <NoteStatus notes={notes}/>
+            <NoteStatus notes={notes} />
             <NoteList
               notes={notes}
               onDeleteNote={handleDelete}
