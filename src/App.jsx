5abc691b9c82892ed5862b8181aa6cb1ee06dee3
afshinmhdz <afshinmhdz1906@gmail.com>
2024-09-02 13:32:48 +0330

    using hook useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,71 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import "../src/App.css";
 import "../src/index.css";
 import AddNewNote from "./component/AddNewNote";
 import NoteList from "./component/NoteList";
 import NoteStatus from "./component/NoteStatus";
 import NoteHeader from "./component/NoteHeader";
+
+function notesReducer(state, { type, payload }) {
+  switch (type) {
+    case "handleAddNote": {
+      return [...state, payload];
+    }
+    case "handelDelete": {
+      return state.filter((item) => item.id !== payload);
+    }
+    case "handleComplete": {
+      return state.map((note) =>
+        note.id === payload ? { ...note, completed: !note.completed } : note
+      );
+    }
+    default:throw new Error ("unknown error"+type)
+  }
+}
 function App() {
-  const [notes, setNotes] = useState([]);
+  const [notes, dispatch] = useReducer(notesReducer, []);
+  //const [notes, setNotes] = useState([]);
   const [sortby, setSortby] = useState("latest");
+
   const handleAddNote = (newNote) => {
-    setNotes((prevNotes) => {
-      return [...prevNotes, newNote];
-    });
+    dispatch({ type: "handleAddNote", payload: newNote });
+
+    // setNotes((prevNotes) => {
+    //   return [...prevNotes, newNote];
+    // });
   };
 
   const handleDelete = (id) => {
-    setNotes(() => notes.filter((item) => item.id !== id));
+    dispatch({ type: "handelDelete", payload: id });
+
+    //setNotes(() => notes.filter((item) => item.id !== id));
   };
   const handleCompleted = (id) => {
-    setNotes((notes) =>
-      notes.map((note) =>
-        note.id === id ? { ...note, completed: !note.completed } : note
-      )
-    );
+    dispatch({ type: "handleComplete", payload: id });
+
+    // setNotes((notes) =>
+    //   notes.map((note) =>
+    //     note.id === id ? { ...note, completed: !note.completed } : note
+    //   )
+    // );
   };
 
   let sortedNotes = notes;
   if (sortby === "earliest")
-    sortedNotes=[...notes].sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+    sortedNotes = [...notes].sort(
+      (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+    );
   {
     /**[...notes] in this line for an shallow copy of array for not mutate state and we can write this line with another way => notes.slice().sort((a,b)=>new Date(a.createdAt)-new Date(b.createdAt));  */
   }
   if (sortby === "latest")
-    sortedNotes=[...notes].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+    sortedNotes = [...notes].sort(
+      (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+    );
   if (sortby === "completed")
-    sortedNotes=[...notes].sort((a, b) => Number((b.completed)) - Number((a.completed)));
+    sortedNotes = [...notes].sort(
+      (a, b) => Number(b.completed) - Number(a.completed)
+    );
 
   return (
     <>
